ae16c461bbbb2f67a4978f8b8e56cb03140ec17b
franselles <fran.selles@gmail.com>
2019-09-14 10:48:15 +0200

    A20190914-A

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -19,7 +19,7 @@
             </form>
           </div>
           <p class="has-text-grey">
-            <a href="../">0.1.23</a> &nbsp;·&nbsp;
+            <a href="../">0.1.24</a> &nbsp;·&nbsp;
           </p>
         </div>
       </div>
